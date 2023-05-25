d689c0eb49d9c6e3e62f1a6815b2c4d3fdf06113
brandket <104182223+brandket@users.noreply.github.com>
2023-05-25 23:54:00 +0300

    Add files via upload

--- forest.py
+++ forest.py
@@ -1,1 +1,25 @@
 from tree import DecisionTreeRegressor
+import numpy as np
+class RandomForestRegressor(DecisionTreeRegressor):
+    def __init__(self, x, y , Ans = 1, rand = 1, *args, **kwargs):
+        super().__init__(x, y, *args, **kwargs)
+        self.trees = []
+        self.rand = rand
+        self.forest_size = Ans
+
+
+    def fit(self, x, y):
+        for i in range(self.forest_size):
+            mask = np.random.random(y.shape[0])
+            x_f, y_f = x[mask < self.rand], y[mask < self.rand] 
+            regr = DecisionTreeRegressor(x_f, y_f, max_depth=self.max_depth)
+            regr.fit(x_f, y_f)
+            self.trees.append(regr)
+
+
+    def predict(self, x):
+        y_pred = np.zeros(x.shape[0], dtype=float)
+        N = self.forest_size
+        for tree in self.trees:
+            y_pred += tree.predict(x)
+        return y_pred/N
--- galaxies.py
+++ galaxies.py
@@ -1,4 +1,56 @@
 from forest import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+import json
+import csv
+import numpy as np
+import os
+import shutil
 
 if __name__ == "__main__":
-	pass
+    data = pd.read_csv('sdss_redshift.csv')
+    
+
+    source_file = 'sdss_redshift.csv'
+    copy_file = 'sdss_predict.csv'
+    shutil.copyfile(source_file, copy_file)
+    
+    
+    
+    x = data.drop(columns = 'redshift').to_numpy()
+    y = data['redshift'].to_numpy()
+    x_tr, x_t, y_tr, y_t = train_test_split(x, y, train_size=0.8,
+                        random_state=42)
+    regr = RandomForestRegressor(x_tr, y_tr, Ans = 1, rand = 0.7)
+    
+    regr.fit(x_tr, y_tr)
+    y_p_t = regr.predict(x_t)
+    y_p_tr = regr.predict(x_tr)
+
+    
+    plt.figure()
+    plt.plot(np.repeat(y_t - y_p_t, 4),'*' , color='indigo', ms = 0.5, label='истинное значение — предсказание на Тесте')
+    plt.plot(y_tr - y_p_tr,'o' ,ms = 0.4, color='orange', label='истинное значение — предсказание на обучающей')
+    plt.legend() 
+    plt.savefig('redhift.png') 
+    plt.show()
+    
+    
+    e1 = mean_squared_error(y_tr, y_p_tr)
+    e2 = mean_squared_error(y_t, y_p_t)
+    
+
+    ans = {"train": e1,"test": e2}
+    with open('redhsift.json', 'w') as f:
+        json.dump(ans, f)
+
+    df = pd.read_csv('sdss.csv')
+    x = df.to_numpy()
+    y_pred =regr.predict(x)
+
+    dk = pd.read_csv('sdss_predict.csv')
+    dk['redshift'] = y_pred
+ 
